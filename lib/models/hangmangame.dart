a24f042a2e211a19cd34023867e04d2d077d2edb
NoFlo911 <noah.flores02@utrgv.edu>
2022-09-27 00:27:18 -0500

    finish part 2

--- lib/models/hangmangame.dart
+++ lib/models/hangmangame.dart
@@ -26,14 +26,59 @@ class HangmanGame {
 
   bool guess(String letter) {
     // TODO: Fill this in
+    if (letter == null || letter.length > 1) {
+      throw ArgumentError();
+    } else if (_word.contains(letter.toLowerCase()) &&
+        !_correctGuesses.contains(letter.toLowerCase()) &&
+        ((letter.codeUnitAt(0) >= 65 && letter.codeUnitAt(0) <= 90) ||
+            (letter.codeUnitAt(0) >= 97 && letter.codeUnitAt(0) <= 122))) {
+      _correctGuesses += letter;
+    } else if (!_word.contains(letter.toLowerCase()) &&
+        !_wrongGuesses.contains(letter.toLowerCase()) &&
+        ((letter.codeUnitAt(0) >= 65 && letter.codeUnitAt(0) <= 90) ||
+            (letter.codeUnitAt(0) >= 97 && letter.codeUnitAt(0) <= 122))) {
+      _wrongGuesses += letter;
+    } else if ((_word.contains(letter.toLowerCase()) &&
+            _correctGuesses.contains(letter.toLowerCase()) &&
+            ((letter.codeUnitAt(0) >= 65 && letter.codeUnitAt(0) <= 90) ||
+                (letter.codeUnitAt(0) >= 97 && letter.codeUnitAt(0) <= 122))) ||
+        (!_word.contains(letter.toLowerCase()) &&
+            _wrongGuesses.contains(letter.toLowerCase()) &&
+            ((letter.codeUnitAt(0) >= 65 && letter.codeUnitAt(0) <= 90) ||
+                (letter.codeUnitAt(0) >= 97 && letter.codeUnitAt(0) <= 122)))) {
+      return false;
+    } else if ((letter.codeUnitAt(0) > 90 && letter.codeUnitAt(0) < 97) ||
+        (letter.codeUnitAt(0) > 122) ||
+        (letter.codeUnitAt(0) < 65)) {
+      throw ArgumentError();
+    }
+    return true;
   }
 
   String blanksWithCorrectGuesses() {
     // TODO: Fill this in
+    String finalanswer = "";
+    for (int i = 0; i < _word.length; i++) {
+      if (_correctGuesses.contains(_word[i])) {
+        finalanswer += _word[i];
+      } else {
+        finalanswer += '-';
+      }
+    }
+    return finalanswer;
   }
 
   String status() {
     // TODO: Fill this in
+    String playerstatus;
+    if (blanksWithCorrectGuesses() == _word) {
+      playerstatus = "win";
+    } else if (_wrongGuesses.length == 7) {
+      playerstatus = "lose";
+    } else {
+      playerstatus = "play";
+    }
+    return playerstatus;
   }
 
   //when running integration tests always return "banana"
